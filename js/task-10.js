cffa9ef5111176850bfb3455f11a9424324d46a6
Sergey Korobka <nejdancs@gmail.com>
2022-05-13 23:48:31 +0300

    add range and base option in task-19

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,7 @@
 const refs = {
-  inputNumber: document.querySelector("#controls input"),
+  inputNumber: document.querySelector("#controls input[type='number']"),
+  inputStep: document.querySelector("#controls input[type='range']"),
+  outputText: document.querySelector("#text"),
   createBtn: document.querySelector("[data-create]"),
   destroyBtn: document.querySelector("[data-destroy]"),
   sortBtn: document.querySelector("[data-sort]"),
@@ -13,6 +15,8 @@ function getRandomHexColor() {
 function onCreateBtn() {
   const amount = refs.inputNumber.value;
   const sizeInit = 30;
+  const sizeStep = Number(refs.inputStep.value);
+
   const boxes = [];
   let size = parseInt(
     refs.outputBoxes.classList.contains("from-big")
@@ -24,7 +28,7 @@ function onCreateBtn() {
     if (isNaN(size)) {
       size = sizeInit;
     } else {
-      size += 10;
+      size += sizeStep;
     }
 
     boxes.push(createBox(size));
@@ -91,6 +95,60 @@ function onSortBtn(event) {
   refs.outputBoxes.append(...sortBox());
 }
 
+function handleInputStep() {
+  refs.outputText.textContent = `${refs.inputStep.value}px`;
+}
+
+refs.inputStep.addEventListener("input", handleInputStep);
 refs.createBtn.addEventListener("click", onCreateBtn);
 refs.destroyBtn.addEventListener("click", onDestroyBtn);
 refs.sortBtn.addEventListener("click", onSortBtn);
+
+// ====================================Базовый вариант=====================================================
+// const refs = {
+//   inputNumber: document.querySelector("#controls input"),
+//   createBtn: document.querySelector("[data-create]"),
+//   destroyBtn: document.querySelector("[data-destroy]"),
+//   outputBoxes: document.querySelector("#boxes"),
+// };
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// function onCreateBtn() {
+//   const amount = refs.inputNumber.value;
+//   const sizeInit = 30;
+//   const boxes = [];
+//   let size = parseInt(refs.outputBoxes.lastElementChild?.style.width);
+
+//   for (let i = 1; i <= amount; i += 1) {
+//     if (isNaN(size)) {
+//       size = sizeInit;
+//     } else {
+//       size += 10;
+//     }
+
+//     boxes.push(createBox(size));
+//   }
+
+//   refs.outputBoxes.append(...boxes);
+// }
+
+// function createBox(size) {
+//   const box = document.createElement("div");
+//   box.style.backgroundColor = getRandomHexColor();
+//   box.style.width = `${size}px`;
+//   box.style.height = `${size}px`;
+//   box.classList.add("box");
+//   box.textContent = `${size}px`;
+
+//   return box;
+// }
+
+// function onDestroyBtn() {
+//   refs.outputBoxes.innerHTML = "";
+// }
+
+// refs.createBtn.addEventListener("click", onCreateBtn);
+// refs.destroyBtn.addEventListener("click", onDestroyBtn);
